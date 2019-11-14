8f0d0b72d32fe4552efafe775087894c1e5fe5ed
garridola_sd2023@CEACCS.NET <garridola_sd2023@CEACCS.NET>
2019-11-14 17:04:41 +0800

    Tabs added

--- src/components/modules/Menu.vue
+++ src/components/modules/Menu.vue
@@ -1,22 +1,17 @@
 <template>
-    <v-navigation-drawer v-model="drawer" absolute temporary style="z-index:2">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title text-center">Menu</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list nav>
-        <v-list-item v-for="item in items" :key="item.title" :to="item.link">
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>           
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <v-tabs
+      id="tabs"
+      color="blue accent-4" right
+    >
+      <v-tab v-for="item in items" :key="item.title" :to="item.link">{{item.title}}</v-tab>
+
+    </v-tabs>
 </template>
+<style scoped>
+#tabs{
+  width: 50%;
+}
+</style>
 <script>
 import Vue from "vue";
 export default {
@@ -26,14 +21,7 @@ export default {
       { title: "Barangay Indigency", icon: "mdi-home", link:'/barangay-indigency'},
       { title: "Business Clearance", icon: "mdi-home", link:'/business-clearance'},
       { title: "Home", icon: "mdi-home", link:'/home'}
-    ],
-    drawer: false,
-    group: null
-  }),
-  watch: {
-    group() {
-      this.drawer = false;
-    }
-  }
+    ]
+  })
 };
 </script>
--- src/components/modules/NavBar.vue
+++ src/components/modules/NavBar.vue
@@ -2,66 +2,20 @@
   <v-container>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
         <span>Express</span>
         <span class="font-weight-light">_docx</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text href="#">
-        <span class="mr-2">Beta Version</span>
-      </v-btn>
+      <Menu/>
     </v-app-bar>
-    <br />
-    <v-navigation-drawer v-model="drawer" absolute temporary style="z-index:2">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title text-center">Menu</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list nav>
-        <v-list-item v-for="item in items" :key="item.title" :to="item.link">
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
   </v-container>
 </template>
 <script>
+import Menu from "./Menu.vue";
 import Vue from "vue";
 export default {
-  data() {
-    return {
-      items: [
-        {
-          title: "Barangay Clearance",
-          icon: "mdi-home",
-          link: "/barangay-clearance"
-        },
-        {
-          title: "Barangay Indigency",
-          icon: "mdi-home",
-          link: "/barangay-indigency"
-        },
-        {
-          title: "Business Clearance",
-          icon: "mdi-home",
-          link: "/business-clearance"
-        },
-        { title: "Home", icon: "mdi-home", link: "/home" }
-      ],
-      drawer: false,
-      group: null
-    };
-  },
-  watch: {
-    group() {
-      this.drawer = false;
-    }
+  components: {
+    Menu
   }
-};
+}
 </script>
